0faeaa2c221a98d9e940b2cbab5ae81e986b56cf
siddhantshingi <snshingi19@gmail.com>
2021-11-16 02:57:29 -0500

    Added GAT code + structure feature update

--- scripts/baselines_run.sh
+++
@@ -1,6 +0,0 @@
-python3 baselines.py --dataset cora --model_tag ladies_64_cora > ./summary.txt
-mv ./summary.txt ../results/ladies_64_cora/summary.txt
-python3 baselines.py --dataset citeseer --model_tag ladies_64_citeseer > ./summary.txt
-mv ./summary.txt ../results/ladies_64_citeseer/summary.txt
-python3 baselines.py --dataset pubmed --model_tag ladies_64_pubmed > ./summary.txt
-mv ./summary.txt ../results/ladies_64_pubmed/summary.txt
